a631d7d7dbf840181c49f7e2f0a5ace0d051333e
visitter <ivanzhotev@gmail.com>
2014-10-07 00:28:10 +0300

    Lect5

---
+++ iz_jf_exercises/src/Lecture5/Lecture5Main.java
@@ -0,0 +1,13 @@
+package Lecture5;
+
+import javax.swing.JFrame;
+
+public class Lecture5Main {
+
+	public static void main(String[] args) {		
+		CarForm carForm = new CarForm();
+		carForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+		carForm.setLocationRelativeTo(null);
+		carForm.setVisible(true);		
+	}
+}
