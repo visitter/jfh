006158c066c0c0217c6afa23c5c60ccde04ed13a
visitter <ivanzhotev@gamil.com>
2014-10-15 00:00:01 +0300

    quizmain

---
+++ iz_jf_exercises/src/Lecture6Exercising/QuizMain.java
@@ -0,0 +1,15 @@
+package Lecture6Exercising;
+
+import javax.swing.JFrame;
+
+public class QuizMain {
+
+	public static void main(String[] args) {		
+		QuizGeneratorFrame mainWindow = new QuizGeneratorFrame();
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainWindow.setLocationRelativeTo(null);
+		mainWindow.setVisible(true);
+
+	}
+
+}
