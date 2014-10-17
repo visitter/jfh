84d2652ed7cb2d0f68f38f3050f4703a00adb07f
visitter <ivanzhotev@gmail.com>
2014-10-17 15:57:59 +0300

    17.10.2014_work

--- iz_jf_exercises/src/Lecture6Exercising/QuizMain.java
+++ iz_jf_exercises/src/Lecture6Exercising/QuizMain.java
@@ -5,11 +5,15 @@ import javax.swing.JFrame;
 public class QuizMain {
 
 	public static void main(String[] args) {		
+		/*
 		QuizGeneratorFrame mainWindow = new QuizGeneratorFrame();
 		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainWindow.setLocationRelativeTo(null);
 		mainWindow.setVisible(true);
-
+		*/
+		QuizFrame mainWindow = new QuizFrame();
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainWindow.setLocationRelativeTo(null);
+		mainWindow.setVisible(true);
 	}
-
 }
